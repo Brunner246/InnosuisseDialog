f4fe66b2583e3f3000d511c807b739782a0db352
Michael Brunner <brunner@cadwork.swiss>
2023-12-05 14:28:24 +0100

    - aggregate model classes

--- PluginEntry.py
+++ PluginEntry.py
@@ -4,7 +4,6 @@ import sys
 import utility_controller as uc
 from PyQt5.QtWidgets import QWidget
 
-DEBUG = True
 PLUGIN_PATH = uc.get_plugin_path()
 
 sys.path.append(PLUGIN_PATH)
@@ -13,6 +12,10 @@ from Controller.SimpleTextController import SimpleTextController
 from Controller.ComplexTextController import ComplexTextController
 from Model.Page1Model import Page1Model
 from Model.Page2Model import Page2Model
+from Model.BaseModel import BaseModel
+
+# DEBUG TOGGLE
+DEBUG = False
 
 if DEBUG:
     sys.path.append(r'D:\Python\PycharmProjects\InnosuisseDialogEnv\Lib\site-packages')
@@ -22,9 +25,13 @@ if DEBUG:
 if __name__ == "__main__":
     window_handle = uc.get_3d_hwnd()
     parent_widget = QWidget.find(window_handle)
+
     simple_controller = SimpleTextController()
     complex_controller = ComplexTextController()
-    model1 = Page1Model(complex_controller)
-    model2 = Page2Model(simple_controller)
-    parent_dialog = ParentDialog(model1, model2, parent_widget)
+
+    base_model = BaseModel()
+    base_model.model1 = Page1Model(complex_controller)
+    base_model.model2 = Page2Model(simple_controller)
+
+    parent_dialog = ParentDialog(base_model, parent_widget)
     parent_dialog.show()
--- View/ParentDialog.py
+++ View/ParentDialog.py
@@ -7,27 +7,26 @@ from PyQt5.QtCore import Qt
 import sys
 import utility_controller as uc
 
+
 PLUGIN_PATH = uc.get_plugin_path()
 sys.path.append(PLUGIN_PATH)
 
 from View.ParentDialog_ui import Ui_Dialog
 from View.ChildPage1 import ChildPage1
 from View.ChildPage2 import ChildPage2
-from Model.Page1Model import Page1Model
-from Model.Page2Model import Page2Model
+from Model.BaseModel import BaseModel
 
 
 class ParentDialog(QDialog, Ui_Dialog):
-    def __init__(self, model1: Page1Model, model2: Page2Model, parent=None):
+    def __init__(self, model: BaseModel, parent=None):
         super().__init__(parent)
         self.setupUi(self)
         self.set_window_flags()
-
-        self.__model1 = model1
-        self.__model2 = model2
-        self.__page_1 = ChildPage1(self.__model1)
-        self.__page_2 = ChildPage2(self.__model2)
+        self.__model = model
+        self.__page_1 = ChildPage1(self.__model.model1, self)
+        self.__page_2 = ChildPage2(self.__model.model2, self)
         self.__add_tab_widgets()
+        self.setAttribute(Qt.WA_DeleteOnClose)
 
     def __add_tab_widgets(self):
         self.tabWidget.addTab(self.__page_1, "Complex Text Object")
